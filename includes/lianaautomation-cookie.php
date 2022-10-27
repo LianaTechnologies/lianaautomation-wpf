50584fb8de351b902acaea61bac5ced3ca9b1f6f
Jaakko Pero <jaakko.pero@lianatech.com>
2022-10-27 14:46:53 +0300

    continued coding standards compatibility work - added license and readme

--- includes/lianaautomation-cookie.php
+++ includes/lianaautomation-cookie.php
@@ -4,38 +4,34 @@
  *
  * PHP Version 7.4
  *
- * @category Components
- * @package  WordPress
- * @author   Liana Technologies <websites@lianatech.com>
- * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later 
+ * @package  LianaAutomation
+ * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later
  * @link     https://www.lianatech.com
  */
-if (!function_exists('Liana_Automation_cookie')) {
-    /**
-     * Cookie Function
-     *
-     * Provides liana_t cookie functionality
-     *
-     * @return null
-     */
-    function Liana_Automation_cookie()
-    {
-        // Generates liana_t tracking cookie if not set
-        if (isset($_COOKIE['liana_t'])) {
-            $liana_t = $_COOKIE['liana_t'];
-            // error_log("liana_t cookie found: ".$liana_t);
-        } else {
-            $liana_t = uniqid('', true);
-            setcookie(
-                'liana_t',
-                $liana_t,
-                time() + 315569260,
-                COOKIEPATH,
-                COOKIE_DOMAIN
-            );
-            // error_log("liana_t cookie set: ".$liana_t);
-        }
-    }
 
-    add_action('wp_head', 'Liana_Automation_cookie', 1, 0);
+if ( ! function_exists( 'liana_automation_cookie' ) && ! function_exists( 'Liana_Automation_cookie' ) ) {
+	/**
+	 * Cookie Function
+	 *
+	 * Provides liana_t cookie functionality
+	 *
+	 * @return void
+	 */
+	function liana_automation_cookie(): void {
+		// Generates liana_t tracking cookie if not set.
+		if ( isset( $_COOKIE['liana_t'] ) ) {
+			$liana_t = sanitize_key( $_COOKIE['liana_t'] );
+		} else {
+			$liana_t = uniqid( '', true );
+			setcookie(
+				'liana_t',
+				$liana_t,
+				time() + 315569260,
+				COOKIEPATH,
+				COOKIE_DOMAIN
+			);
+		}
+	}
+
+	add_action( 'wp_head', 'liana_automation_cookie', 1, 0 );
 }
--- lianaautomation-wpforms.php
+++ lianaautomation-wpforms.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * Plugin Name:       LianaAutomation - WPForms
- * Plugin URI:        https://www.lianatech.com/solutions/websites
+ * Plugin Name:       LianaAutomation WPF
  * Description:       LianaAutomation for WP Forms.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires at least: 5.2
  * Requires PHP:      7.4
- * Author:            Liana Technologies
+ * Author:            Liana Technologies Oy
  * Author URI:        https://www.lianatech.com
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * Text Domain:       lianaautomation
  * Domain Path:       /languages
- * 
+ *
  * PHP Version 7.4
- * 
- * @category Components
- * @package  WordPress
+ *
+ * @package  LianaAutomation
  * @author   Liana Technologies <websites@lianatech.com>
- * @author   Jaakko Pero <jaakko.pero@lianatech.com>
  * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later
  * @link     https://www.lianatech.com
  */
